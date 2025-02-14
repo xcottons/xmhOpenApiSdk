6d2bf6ca2d35d9a17aea08e51455e3dcd336c195
yujianfx <yujianfx@shouhui-tech.com>
2025-02-14 18:42:21 +0800

    fix

--- Dorder.go
+++ Dorder.go
@@ -26,20 +26,21 @@ type DOrder struct {
 	OrderId           string         `json:"orderId"`
 	SubOrderId        string         `json:"subOrderId,omitempty"`
 	Currency          string         `json:"currency" validate:"required,currency"`
-	TotalPrice        string         `json:"totalPrice" validate:"required,numeric"`                 //总金额，总金额(含税费，含运费) - 优惠金额 = 实际支付金额，以元为单位
-	PreferentialPrice string         `json:"preferentialPrice" validate:"required,numeric"`          //优惠金额，以元为单位，订单的优惠金额。
-	TaxPrice          string         `json:"taxPrice" validate:"omitempty,numeric"`                  //税费，可能是增值税可能是其他税，以元为单位
-	ShipPrice         string         `json:"shipPrice" validate:"omitempty,numeric"`                 //运费，以元为单位
-	TotalPayPrice     string         `json:"totalPayPrice" validate:"omitempty,numeric"`             //实际支付金额，以元为单位
-	InsuredPayPrice   string         `json:"insuredPayPrice,omitempty" validate:"omitempty,numeric"` //邮包险支付金额，以元为单位 （邮包险）
-	PayTime           string         `json:"payTime" validate:"omitempty"`                           //支付时间
+	TotalPrice        string         `json:"totalPrice" validate:"required,numeric"`
+	PreferentialPrice string         `json:"preferentialPrice" validate:"required,numeric"`
+	TaxPrice          string         `json:"taxPrice" validate:"omitempty,numeric"`
+	ShipPrice         string         `json:"shipPrice" validate:"omitempty,numeric"`
+	TotalPayPrice     string         `json:"totalPayPrice" validate:"omitempty,numeric"`
+	InsuredPayPrice   string         `json:"insuredPayPrice,omitempty" validate:"omitempty,numeric"`
+	PayTime           string         `json:"payTime" validate:"omitempty"`
 	ItemList          []*DItem       `json:"itemList"`
-	OrderState        OrderState     `json:"orderState"`                           //同步平台订单时需要判断是否为空
-	SenderInfo        *SenderInfoDto `json:"senderInfo"`                           //validate:"required" 试算的时候不要求填写，同步订单数据的时候才要求。
-	*ReceiverInfoDto  `json:"receiverInfo"`                                        //validate:"required" 同上。
-	OrderCreateTime   string         `json:"orderCreateTime" validate:"omitempty"` //同步平台订单时需要判断是否为空
-	OrderModifyTime   string         `json:"orderModifyTime" validate:"omitempty"` //同步平台订单时需要判断是否为空
-	IsHistoryOrder    bool           `json:"isHistoryOrder"`                       //是否历史订单，默认为false
+	OrderState        OrderState     `json:"orderState"`
+	SenderInfo        *SenderInfoDto `json:"senderInfo"`
+	*ReceiverInfoDto  `json:"receiverInfo"`
+	OrderCreateTime   string      `json:"orderCreateTime" validate:"omitempty"`
+	OrderModifyTime   string      `json:"orderModifyTime" validate:"omitempty"`
+	IsHistoryOrder    bool        `json:"isHistoryOrder"`
+	ShipInfoList      []*ShipInfo `json:"shipInfoList"`
 }
 
 type SenderInfoDto struct {
